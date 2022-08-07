fdadca6428cee493fd3ec3ee240757ba53dc337f
Daniel Rama <danielrama017@gmail.com>
2022-08-07 21:31:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
